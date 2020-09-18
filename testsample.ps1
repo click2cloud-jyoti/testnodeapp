c9181fb5e01bbaa73cb901789701521719fd014e
Jyoti Tumsare <jyoti.tumsare@click2cloud.net>
2020-09-18 16:06:07 +0530

    Update template

--- testsample.ps1
+++ testsample.ps1
@@ -1,9 +1,16 @@
 Param(
 [string]$test_env1,
-[string]$test_env2
-
-
+[string]$test_env1
 )
+
+
+Write-Host "Starting PS Task 1"
+
+Write-Host "Our public variable is: $($env:PublicVariable)";
+Write-Host ("##vso[task.setvariable variable=PublicVariable]Teapots don't exist in space")
+Write-Host "Our altered public variable is: $($env:PublicVariable)";
+
+Write-Host "PS Task 1 Completed"
 Write-Output "sauce.userName value from ADO was passed as a Argument in the ADO Task called $env:TEST_ENV1 " +
 "to sauceUserName variable in the Posh. This is the value found=>$test_env1"
 [Environment]::SetEnvironmentVariable("TEST_ENV1", "$test_env1", "User")
