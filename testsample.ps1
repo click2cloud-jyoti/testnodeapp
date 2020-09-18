99576be2f1969a00d44f5a098862e2ca889f0404
Jyoti Tumsare <jyoti.tumsare@click2cloud.net>
2020-09-18 09:27:17 +0530

    Update testsample.ps1

--- testsample.ps1
+++ testsample.ps1
@@ -1,7 +1,7 @@
 Param(
 [string]$test_env1,
-[string]$test_env2,
-[integer]$node_port
+[string]$test_env2
+
 
 )
 Write-Output "sauce.userName value from ADO was passed as a Argument in the ADO Task called $env:TEST_ENV1 " +
@@ -12,7 +12,4 @@ Write-Output "sauce.userName value from ADO was passed as a Argument in the ADO 
 "to sauceUserName variable in the Posh. This is the value found=>$test_env2"
 [Environment]::SetEnvironmentVariable("TEST_ENV2", "$test_env2", "User")
 
-Write-Output "sauce.userName value from ADO was passed as a Argument in the ADO Task called $env:TEST_ENV2 " +
-"to sauceUserName variable in the Posh. This is the value found=>$test_env2"
-[Environment]::SetEnvironmentVariable("NODE_PORT", "$node_port", "User")
 
